d2786dd53e14f0b9a236639b09a85b88922f26e9
GrecoBen <greco.benjamin@orange.fr>
2023-05-02 20:23:36 +0200

    CV

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(event){
+    //* On execute la méthode init du module destinationMOdule pour ajouter le écouteur d'evenement sur les bouton coeur
+    themeModule.init();
+})
--- js/theme.js
+++ js/theme.js
@@ -1,32 +1,58 @@
+/*const themeModule = {
+
+init: function () {
+
+  //* La même chose mais en localStorage
+  const defaultTheme = localStorage.getItem('dark-mode');
+  
+  if(defaultTheme != null){
+      themeModule.switchTheme(defaultTheme);
+  }
+
+  if (defaultTheme === 'dark') {
+      themeModule.toggleDarkMode();
+  }
+}
+}  */
+
 function toggleDarkMode() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+    if (element.classList.contains("dark-mode")) {
+  localStorage.setItem("darkModeEnabled", "true");
+} else {
+  localStorage.setItem("darkModeEnabled", "false");
+} console.log(localStorage);
+}
 
-  function toggleButtonHover() {
-    let element = document.getElementsByClassName("button");
-    element.classList.toggle("hover");
-  }
+function toggleButtonHover() {
+  let element = document.querySelector('button');
+  element.classList.toggle("hover");
+}
 
-  function toggleButtonDelay() {
-    let element = document.getElementsByClassName("button");
-    element.classList.add("button-delay");
-    setTimeout(function() {
-      element.classList.remove("button-delay");
-    }, 2000); 
-  }
+function toggleButtonDelay() {
+  let element = document.querySelector('button');
+  element.classList.add("button-delay");
+  setTimeout(function () {
+    element.classList.remove("button-delay");
+  }, 5000);
+}
 
-  function toggleDarkMode() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-    let divs = document.getElementsByTagName("div");
-    for (let i = 0; i < divs.length; i++) {
-      divs[i].classList.toggle("dark-mode-div");
-    }
-    let button = document.querySelector("button");
+function toggleDarkMode() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+  let divs = document.getElementsByTagName("div");
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].classList.toggle("dark-mode-div");
+  }
+  let button = document.querySelector("button");
   if (element.classList.contains("dark-mode")) {
     button.style.backgroundColor = "white";
   } else {
     button.style.backgroundColor = "black";
   }
-  }
+}
+
+
+
+
